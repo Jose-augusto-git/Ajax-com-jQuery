56e745531b23d9db9f0f5d298d43430052575230
José Augusto <73261477+Jose-augusto-git@users.noreply.github.com>
2021-11-15 15:40:54 -0300

    3.0

--- app.php
+++ app.php
@@ -87,20 +87,116 @@ class Bd {
 
     }
 
+    public function getTotalVendas(){
+
+        $query = '
+
+            SELECT SUM(total) AS total_vendas
+            FROM tb_vendas
+            WHERE data_venda BETWEEN :data_inicio AND :data_fim
+        
+        ';
+
+        $stmt = $this->conexao->prepare($query);
+        
+        $stmt->bindValue(':data_inicio', $this->dashboard->__get('data_inicio'));
+        $stmt->bindValue(':data_fim', $this->dashboard->__get('data_fim'));
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_OBJ)->total_vendas;
+
+    }
+
+    public function getClientesAtivos(){
+
+        $query = '
+
+        SELECT COUNT(*) AS clientes_ativos
+        FROM tb_clientes
+        WHERE cliente_ativo = true AND data_clientes BETWEEN :data_inicio AND :data_fim
+        
+        ';
+
+        $stmt = $this->conexao->prepare($query);     
+        
+        $stmt->bindValue(':data_inicio', $this->dashboard->__get('data_inicio'));
+        $stmt->bindValue(':data_fim', $this->dashboard->__get('data_fim'));
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_OBJ)->clientes_ativos;
+
+    }
+
+    public function getClientesInativos(){
+
+        $query = '
+
+        SELECT COUNT(*) AS clientes_inativos
+        FROM tb_clientes
+        WHERE cliente_ativo = false AND data_clientes BETWEEN :data_inicio AND :data_fim
+        
+        ';
+
+        $stmt = $this->conexao->prepare($query);     
+
+        $stmt->bindValue(':data_inicio', $this->dashboard->__get('data_inicio'));
+        $stmt->bindValue(':data_fim', $this->dashboard->__get('data_fim'));
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_OBJ)->clientes_inativos;
+
+    }
+
+    public function getTotalDespesas(){
+
+        $query = '
+
+            SELECT SUM(total) AS total_despesas
+            FROM tb_despesas
+            WHERE data_despesa BETWEEN :data_inicio AND :data_fim
+        
+        ';
+
+        $stmt = $this->conexao->prepare($query);
+        
+        $stmt->bindValue(':data_inicio', $this->dashboard->__get('data_inicio'));
+        $stmt->bindValue(':data_fim', $this->dashboard->__get('data_fim'));
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_OBJ)->total_despesas;
+
+    }
+
 }
 
 //lógica do script
 $dashboard = new Dashboard();
 $conexao = new Conexao();
 
-$dashboard->__set('data_inicio', '');
-$dashboard->__set('data_fim', '');
+$competencia = explode('-', $_GET['competencia']);
+$ano = $competencia[0];
+$mes = $competencia[1];
+
+$dias_do_mes = cal_days_in_month(CAL_GREGORIAN, $mes, $ano );
+
+$dashboard->__set('data_inicio', $ano. '-' .$mes. '-01');
+$dashboard->__set('data_fim', $ano. '-' .$mes. '-' .$dias_do_mes);
 
 
 $bd = new Bd($conexao, $dashboard);
 
 $dashboard->__set('numeroVendas', $bd->getNumeroVendas());
-print_r($dashboard);
+$dashboard->__set('totalVendas', $bd->getTotalVendas());
+$dashboard->__set('clientesAtivos', $bd->getClientesAtivos());
+$dashboard->__set('clientesInativos', $bd->getClientesInativos());
+$dashboard->__set('totalDespesas', $bd->getTotalDespesas());
+
+
+
+
+echo json_encode($dashboard);
+
+
 
 
 
--- js/script.js
+++ js/script.js
@@ -26,4 +26,36 @@ $(document).ready(() => {
             $('#pagina').html(data)
         })
     })
+
+    //ajax 
+    $('#competencia').on('change', e => {
+
+        let competencia = $(e.target).val()
+
+        $.ajax({
+            type: 'GET',
+            url: 'app.php',
+            data: `competencia=${competencia}`,//síntese: x-www-form-urlencoded
+            dataType: 'json',
+            success:  (dados) => {
+
+                $('#numeroVendas').html(dados.numeroVendas),
+                $('#totalVendas').html(dados.totalVendas),
+                $('#clientesAtivos').html(dados.clientesAtivos)
+                $('#clientesInativos').html(dados.clientesInativos)
+                $('#totalDespesas').html(dados.totalDespesas)
+
+
+
+
+
+            },
+            error: (erro) => {console.log(erro)}
+        })
+
+        //métodos, url, dados, sucesso, erro
+
+    })
+
+
 })
